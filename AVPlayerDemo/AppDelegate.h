e0e82204a2a09fd87b06084e60c85541acf0f09b
maruiduan <358043769@qq.com>
2014-02-11 17:05:59 +0800

    Initial Commit

---
+++ AVPlayerDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AVPlayerDemo
+//
+//  Created by maruiduan on 14-2-10.
+//  Copyright (c) 2014年 Ben. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
